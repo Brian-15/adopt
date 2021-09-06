2adbf0b5e528acc41a1196a842e108f39b8d6226
Brian-15 <bcirieco@gmail.com>
2021-09-06 16:49:24 -0700

    changed species form field to a SelectField

--- forms.py
+++ forms.py
@@ -1,13 +1,14 @@
 from typing import Optional
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField
+from wtforms.fields.core import SelectField
 from wtforms.validators import InputRequired, URL, NumberRange, Optional
 
 class AddPetForm(FlaskForm):
     """Form class for adding a pet"""
 
     name        = StringField("Name", validators=[InputRequired()])
-    species     = StringField("Species", validators=[InputRequired()])
+    species     = SelectField("Species", choices=[("cat", "Cat"), ("dog", "Dog"), ("porcupine", "Porcupine")], validators=[InputRequired()])
     photo_url   = StringField("Photo URL", validators=[Optional(), URL()])
     age         = IntegerField("Age", validators=[Optional(), NumberRange(min=0, max=30)])
     notes       = StringField("Notes", validators=[Optional()])
