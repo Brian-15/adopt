4262fccb6a349caf34c3ab1204e85f68a5a8e1d1
Brian-15 <bcirieco@gmail.com>
2021-09-06 12:56:14 -0700

    implemented add, edit, and pet details routes.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from logging import PercentStyle
+from logging import PercentStyle, error
 from flask import Flask, request, render_template, redirect, flash
 from flask.templating import render_template_string
 from pet_model import Pet, db, connect_db
-from forms import AddPetForm
+from forms import AddPetForm, EditPetForm
 from flask_debugtoolbar import DebugToolbarExtension
 from werkzeug.exceptions import HTTPException
 
@@ -30,8 +30,49 @@ def add_pet():
 
     if form.validate_on_submit():
         name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+        available = form.available.data
+
+        pet = Pet(name=name, species=species, photo_url=photo_url,
+                  age=age, notes=notes, available=available)
+        
+        db.session.add(pet)
+        db.session.commit()
+
         flash(f"{name} has been added.")
         return redirect("/")
     
     else:
         return render_template("add_pet.html", form=form)
+
+@app.route("/<int:id>", methods=["GET", "POST"])
+def handle_pet(id):
+    """
+        GET: Displays pet details and edit form.
+        POST: updates pet model.
+    """
+    pet = Pet.query.get_or_404(id)
+    form = EditPetForm()
+
+    if form.validate_on_submit():
+        
+        if form.photo_url.data is not "":
+            pet.photo_url = form.photo_url.data
+            flash(f"{pet.name} photo URL updated.")
+
+        if form.notes.data is not "":
+            pet.notes = form.notes.data
+            flash(f"{pet.name} notes updated.")
+        
+        if form.toggle_available.data:
+            pet.available = not pet.available
+            flash(f"{pet.name} is now " + ("un" if not pet.available else "") + "available")
+        
+        db.session.add(pet)
+        db.session.commit()
+        return redirect("/")
+    else:
+        return render_template("pet.html", form=form, pet=pet)
--- forms.py
+++ forms.py
@@ -1,11 +1,19 @@
+from typing import Optional
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField
+from wtforms.validators import InputRequired, URL, NumberRange, Optional
 
 class AddPetForm(FlaskForm):
 
-    name        = StringField("Name")
-    species     = StringField("Species")
-    photo_url   = StringField("Photo URL")
-    age         = IntegerField("Age")
-    notes       = StringField("Notes")
-    available   = BooleanField("Available?")
+    name        = StringField("Name", validators=[InputRequired()])
+    species     = StringField("Species", validators=[InputRequired()])
+    photo_url   = StringField("Photo URL", validators=[Optional(), URL()])
+    age         = IntegerField("Age", validators=[Optional(), NumberRange(min=0, max=30)])
+    notes       = StringField("Notes", validators=[Optional()])
+    available   = BooleanField("Available?", validators=[InputRequired()], default=True)
+
+class EditPetForm(FlaskForm):
+
+    photo_url   = StringField("Photo URL", validators=[Optional(), URL()])
+    notes       = StringField("Notes", validators=[Optional()])
+    toggle_available   = BooleanField("Toggle Availablility (leave unchecked for no change)", validators=[Optional()])
